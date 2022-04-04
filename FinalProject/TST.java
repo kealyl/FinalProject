9b5a55ca1a60de27d9a85727c3e7dc1dd55bed48
laura <laura@192.168.0.220>
2022-04-04 19:04:59 +0100

    Commit - changes TST class

--- FinalProject/TST.java
+++ FinalProject/TST.java
@@ -1,15 +1,52 @@
+import java.util.List;
+
+import com.google.common.base.Strings;
+
 public class TST {
 	
-	private TSTNode root;
+	private TSTNode rootOfTree;	
+	
+	private enum directionOfNode //tells us direction we came from parent node to reach current node
+	{
+		LEFT,
+		RIGHT,
+		MIDDLE
+	}
 	
 	TST() // constructor - empty TST
 	{
-		this.root = null;
+		this.rootOfTree = null;
 	}
 	
+	TST(final List<String> stops) //creates a TST with stops specified in list
+	{
+		for (String stop: stops)
+		{
+			this.insert(stop);
+		}
+	}
+	
+	private TSTNode insert(TSTNode node, final char[] stopName, final int i)
+	{
+		final char currentCharacter = stopName[i];
+		if(node == null)
+		{
+			node = new TSTNode(currentCharacter);
+		}
+	}
+	
+	public TSTNode insert(final String string)
+	{
+		if((string == null) || (string.isEmpty()))
+		{
+			return null;
+		}
+		rootOfTree = insert(this.rootOfTree, string.toUpperCase().toCharArray(),0);
+		return getRoot();
+	}
 	public TSTNode getRoot() //returns root of TST
 	{
-		return root;
+		return rootOfTree;
 	}
 	
 	public boolean isEmptyTST() //returns true if empty TST
@@ -23,6 +60,7 @@ public class TST {
 			return false;
 		}
 	}
+	
 	
 	
 	
